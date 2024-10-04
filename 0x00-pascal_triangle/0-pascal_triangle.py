6a6e9d194bb21c0c423ac66914a3e9fbf595206d
Ngaukon <josephlucia100@gmail.com>
2024-10-04 19:38:43 +0000

    pascal traiangle correct

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -27,4 +27,3 @@ def pascal_triangle(n):
                 line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         triangle.append(line)  # Add the current row to the triangle
     return triangle  # Return the complete Pascal's triangle
-
